8853885053b468a4709edc8b32e77e2e4b424027
Brayan <mrx.source.mrx@gmail.com>
2026-02-04 15:33:09 -0500

    feat: change Navbar to Sidebar

---
+++ src/components/layout/Sidebar.tsx
@@ -0,0 +1,18 @@
+export function Navbar() {
+  return (
+    <>
+      <div className="navbar">
+        <nav className="nav">
+          <div className="links">
+            <a href="#home">Home</a>
+            <a href="#about">About</a>
+            <a href="#experience">Experience</a>
+            <a href="#projects">Projects</a>
+            <a href="#services">Services</a>
+            <a href="#contact">Contact</a>
+          </div>
+        </nav>
+      </div>
+    </>
+  );
+}
