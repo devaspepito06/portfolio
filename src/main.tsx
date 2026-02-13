93aa1279e4c83817be97bb5077a4a95ff1da968b
Brayan <mrx.source.mrx@gmail.com>
2026-02-13 11:23:45 -0500

    feat: add Router and remove some things

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,13 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import { Navbar } from "./components/layout";
-import { App } from "./App";
+import { BrowserRouter } from "react-router";
+import { Routes } from "./components/Routes";
 import "./styles";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <Navbar />
-    <App />
+    <BrowserRouter>
+      <Routes />
+    </BrowserRouter>
   </StrictMode>,
 );
