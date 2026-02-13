acbf0e3df9ba80e5dd4643659ffd6e474ed1d69c
Brayan <mrx.source.mrx@gmail.com>
2026-02-13 11:21:53 -0500

    feat: add sidebar for rendering in App, not in the main

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import { Sidebar } from "./components/layout";
 import {
   About,
   Contact,
@@ -10,6 +11,7 @@ import {
 export function App() {
   return (
     <>
+      <Sidebar />
       <Home />
       <About />
       <Experience />
